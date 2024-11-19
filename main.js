808496f5d5ef485df0f485733a7e4a100ec0c26a
sivan <sivanhannan7@gmail.com>
2024-11-19 10:08:56 +0100

    update2

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const boardElm = document.querySelector(".board");
-
+const holdingBoardElm = document.querySelector(".holding-board");
 const columns = 5;
 const rows = 5;
 window.onload = function () {
-    for(i = 0; i < columns * rows; i++){
+    //blank img in the board
+    for(let i = 0; i < columns * rows; i++){
         const boardimg = document.createElement("img");
         boardElm.appendChild(boardimg);
         boardimg.src = "./images/blank.jpg";
-     }
+    }
+    //game images in the imgboard
+    for(let i = 0; i < 25; i++){
+        const boardimg = document.createElement("img");
+        holdingBoardElm.appendChild(boardimg);
+        boardimg.src = "./images/3.jpg";
+    }
+
 }
 
